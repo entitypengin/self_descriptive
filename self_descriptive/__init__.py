6bafb15cf2bb18d2a587f42d409964b7453bd3ff
EntityPENGIN <nopedoritos@gmail.com>
2022-12-17 11:15:34 +0900

    Initial commit

---
+++ self_descriptive/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from math import *
+
+
+def int_to_tuple(x):
+    result = []
+    d = floor(log10(x))
+    for i in range(d + 1):
+        result = [x % 10 ** (i + 1) // 10 ** i] + result
+    return result
+
+
+def is_self_descriptive(x):
+    tup = int_to_tuple(x)
+    for i in tup:
+        if tup[i] != tup.count(i):
+            break
+    else:
+        return True
+    return False
+
+
+for num in (2_019, 2_020, 6_210_001_000):
+    print(f"{num} {'is' if is_self_descriptive(num) else 'is not'} self descriptive")
+
+# 2019 is not self descriptive
+# 2020 is self descriptive
+# 6210001000 is self descriptive
